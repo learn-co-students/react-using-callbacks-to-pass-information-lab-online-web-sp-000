80c28236c7936435475805e64cb84de5e5f0f7f0
Irum <irum@Irums-MacBook-Air.local>
2018-12-17 18:36:49 +0000

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Cell extends Component {
-  
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       color: this.props.color
-    }
+    };
   }
-  
+
+  changeColour = () => {
+    const newColour = this.props.handleClick();
+    this.setState({ color: newColour });
+  };
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
-      </div>
-    )
+      <div
+        className="cell"
+        style={{ backgroundColor: this.state.color }}
+        onClick={() => this.changeColour()}
+      />
+    );
   }
-  
 }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class ColorSelector extends Component {
-  
-  makeColorSwatches = () => (
-    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
-    })
-  )
-  
+  makeColorSwatches = () =>
+    [
+      "#F00",
+      "#F80",
+      "#FF0",
+      "#0F0",
+      "#00F",
+      "#508",
+      "#90D",
+      "#FFF",
+      "#000"
+    ].map((str, idx) => {
+      return (
+        <div
+          key={idx}
+          className="color-swatch"
+          style={{ backgroundColor: str }}
+          onClick={() => this.props.selectColour(str)}
+        />
+      );
+    });
+
   render() {
-    return (
-      <div id="colorSelector">
-        {this.makeColorSwatches()}
-      </div>
-    )
+    return <div id="colorSelector">{this.makeColorSwatches()}</div>;
   }
-  
 }
+// update makeColorSwatches to provide the < div > s it is rendering with some way
+// to fire off that method we wrote for Matrix(hint: consider using an event listener
+// and controlling the argument when an event handler is fired)
