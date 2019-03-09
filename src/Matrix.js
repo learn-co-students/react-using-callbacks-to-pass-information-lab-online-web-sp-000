d0e777a09c8528497f2142f4900cbab4fa6c72da
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-09 17:24:15 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -10,6 +10,8 @@ export default class Matrix extends Component {
   }
 
   setColor(str) {
+    // this.methodName.bind(this) <-- THIS IS WHAT I NEED TO FIGURE OUT!!!!!!
+    // "Two ways to bind functions into components."
     // SETTTER, çalled from ColorSelector, to SET the color
     this.setState({ selectedColor: str });
   }
@@ -21,7 +23,7 @@ export default class Matrix extends Component {
 
   genRow = vals =>
     vals.map((val, idx) => (
-      <Cell key={idx} color={val} getColorCallback={this.getColor} />
+      <Cell key={idx} color={val} getColorCallback={this.getColor.bind(this)} />
     ));
 
   genMatrix = () =>
@@ -34,7 +36,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector setColorCallback={this.setColor} />
+        <ColorSelector setColorCallback={this.setColor.bind(this)} />
         <div id="matrix">{this.genMatrix()}</div>
       </div>
     );
