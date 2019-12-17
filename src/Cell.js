11615d5dbc3e86057179a077765fa648cd0ef783
github username <github email address>
2019-12-17 13:40:48 +0000

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -17,8 +17,11 @@ export default class Cell extends Component {
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
-      </div>
+      <div onClick={this.handleClick} className="cell"
+      style={{backgroundColor: this.state.color}}
+ >
+ </div>
+     
     )
   }
   
