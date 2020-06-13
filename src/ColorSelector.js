7716ce47bee756b718d395484972db52aff0f49e
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-06-13 12:02:53 -0400

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
-    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((color, index) => {
+      let callback = () => this.props.setSelectedColor(color) 
+      return <div onClick={callback} key={index} className="color-swatch" style={{backgroundColor: color}}/>
     })
   )
 
