6d833e3d50a16362582d88ad8a3a9812210744c6
jenleebeaver <jen.lee.beaver@gmail.com>
2021-02-02 07:03:46 +0530

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,23 +5,41 @@ import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
 
+  //1. creating a state to keep track of selectedColor
   constructor() {
     super()
+    this.state = {
+      selectedColor: '#FFF'
+    }
+  }
+
+  //2. method to change the state value. This updates selectedColor with newColor 
+  setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor
+    })
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    // these are the values passed into cell 
+    // 4.Passing selectedColor into genRow Cell as a prop so Cell.js knows the current selectedColor 
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor}  />)
   )
 
   genMatrix = () => (
+    // returning map of JSX divs 
+    // then calling genRow which returns a collection of Cell components 
+    // this returns a series of divs containing divs 
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        {/* component. 3. We are passing setSelectedColor as a prop into ColorSelector  */}
+        <ColorSelector setSelectedColor={this.setSelectedColor} />
         <div id="matrix">
+          {/* function call  */}
           {this.genMatrix()}
         </div>
       </div>
